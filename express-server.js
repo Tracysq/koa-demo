392dd3ad5c0ef5d11679bbb70337b5883bbb1846
sunqi <sunqi@xiao100.com>
2021-10-20 19:26:13 +0800

    feat: express response time

--- express-server.js
+++ express-server.js
@@ -1,12 +1,15 @@
 const express = require('express')
+const responseTime = require('response-time')
 
 const app = express()
 
-app.use((req, res, next) =>{
-  const start = new Date()
-  res.locals.start = start
-  next()
-})
+app.use(responseTime())
+
+// app.use((req, res, next) =>{
+//   const start = new Date()
+//   res.locals.start = start
+//   next()
+// })
 
 app.use((req, res, next) => {
   res.write('hello  world')
@@ -18,10 +21,15 @@ app.use((req, res, next) => {
 })
 
 app.use((req, res, next) => {
-  const time = new Date() - res.locals.start
-  console.log(`time: ${time}`)
+  console.log(res.get('X-Response-Time'))
   next()
 })
+
+// app.use((req, res, next) => {
+//   const time = new Date() - res.locals.start
+//   console.log(`time: ${time}`)
+//   next()
+// })
 
 app.listen(5000, () => {
   console.log('正在监听端口 5000')
