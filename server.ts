7289200706f07a2dc797b242817cdf5720d0cfc3
sunqi <sunqi@xiao100.com>
2021-10-20 16:31:46 +0800

    feat: next

--- server.ts
+++ server.ts
@@ -3,7 +3,17 @@ import koa from 'koa'
 const app = new koa()
 
 app.use(async (ctx, next) => {
-  ctx.body = 'hello world'
+  ctx.body = 'hello'
+  await next()
+  ctx.body += ' qdd'
+})
+app.use(async (ctx, next) => {
+  ctx.body += ' world'
+  await next()
+})
+app.use(async (ctx, next) => {
+  ctx.set('Content-Type', 'text/html; charset=utf-8')
+  await next()
 })
 
 app.listen(5000, () => {
