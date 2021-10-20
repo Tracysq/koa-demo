18d7f434dc68a889240c4fc5bd148a0c44029e60
sunqi <sunqi@xiao100.com>
2021-10-20 18:12:29 +0800

    feat: express 模拟 koa 获取耗时

---
+++ express-server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+
+const app = express()
+
+app.use((req, res, next) =>{
+  const start = new Date()
+  res.locals.start = start
+  next()
+})
+
+app.use((req, res, next) => {
+  res.write('hello  world')
+  for (let i = 0; i < 10000; i++) {
+    res.write('hello  world')
+  }
+  res.end()
+  next()
+})
+
+app.use((req, res, next) => {
+  const time = new Date() - res.locals.start
+  console.log(`time: ${time}`)
+  next()
+})
+
+app.listen(5000, () => {
+  console.log('正在监听端口 5000')
+})
